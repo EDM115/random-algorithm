b0a9efed74dc77a5594818363f0328142e004e43
EDM115 <dev@edm115.eu.org>
2024-09-19 15:33:01 +0200

    fix: project source

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
   description="A random number generator based on shuffled English words, their ASCII values, and the current epoch time to create highly randomized outputs",
   long_description=long_description,
   long_description_content_type="text/markdown",
-  url="https://github.com/EDM115/random_algorithm",
+  url="https://github.com/EDM115/random-algorithm",
   author="EDM115",
   license="MIT",
   classifiers=[
@@ -21,10 +21,10 @@ setup(
     "Programming Language :: Python :: 3"
   ],
   project_urls={
-    "Documentation": "https://github.com/EDM115/random_algorithm",
+    "Documentation": "https://github.com/EDM115/random-algorithm",
     "Funding": "https://github.com/EDM115#support-me-",
-    "Source": "https://github.com/EDM115/random_algorithm",
-    "Tracker": "https://github.com/EDM115/random_algorithm/issues",
+    "Source": "https://github.com/EDM115/random-algorithm",
+    "Tracker": "https://github.com/EDM115/random-algorithm/issues",
   },
   packages=find_packages(),
   include_package_data=True,
